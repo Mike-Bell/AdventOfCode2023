e37be9a318456e339c6aeb001d4da52146542637
Mike-Bell <mikel3377@gmail.com>
2023-12-08 07:32:19 -0500

    day 8

--- 08/solution.js
+++ 08/solution.js
@@ -1,7 +1,44 @@
-const parseInput = input => input;
+const lcm = require('../utils/lcm');
 
-const runPart1 = input => input;
+const parseInput = input => {
+   const lines = input.split('\r\n');
+   const instructions = lines[0].split('').map(c => c === 'L' ? 0 : 1);
+   const nodes = {};
+   for (const line of lines.slice(2)) {
+      nodes[line.slice(0, 3)] = [line.slice(7, 10), line.slice(12, 15)];
+   }
 
-const runPart2 = input => input;
+   return {instructions, nodes};
+};
+
+const runPart1 = input => {
+   const {instructions, nodes} = input;
+   let node = 'AAA';
+   for (let i = 0; i < Infinity; i++) {
+      node = nodes[node][instructions[i % instructions.length]];
+      if (node === 'ZZZ') {
+         return i + 1;
+      }
+   }
+   return -1;
+};
+
+const runPart2 = input => {
+   const {instructions, nodes} = input;
+   const curNodes = Object.keys(nodes).filter(key => key[2] === 'A');
+   const totalSteps = [];
+   for (let n = 0; n < curNodes.length; n++) {
+      let node = curNodes[n];
+      for (let i = 0; i < Infinity; i++) {
+         node = nodes[node][instructions[i % instructions.length]];
+         if (node[2] === 'Z') {
+            totalSteps[n] = i + 1;
+            break;
+         }
+      }
+   }
+
+   return totalSteps.reduce((acc, curr) => lcm(acc, curr));
+};
 
 module.exports = {parseInput, runPart1, runPart2};
