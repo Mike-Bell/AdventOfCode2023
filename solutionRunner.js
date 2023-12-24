3b8f42ab1fa1ed53fc5182e14027c665c9c3f6a7
Mike-Bell <mikel3377@gmail.com>
2023-12-24 09:04:21 -0500

    day 24

--- solutionRunner.js
+++ solutionRunner.js
@@ -9,7 +9,7 @@ if (!solutionArg) {
 
 console.time('full run');
 
-const runDay = (day, partOrNot) => {
+const runDay = async (day, partOrNot) => {
    if (day.length === 1) {
       day = `0${day}`;
    }
@@ -17,37 +17,42 @@ const runDay = (day, partOrNot) => {
    const input = fs.readFileSync(`./${day}/input.txt`, 'utf8');
    const solutionRunner = require(`./${day}/solution.js`);
 
-   const run = part => {
+   const run = async part => {
       console.time(`solution ${part}`);
       let parsedInput = input;
       if (solutionRunner.parseInput) {
          parsedInput = solutionRunner.parseInput(input);
       }
-      console.log(solutionRunner[`runPart${part}`](parsedInput));
+      let sln = solutionRunner[`runPart${part}`](parsedInput);
+      if (sln.then) {
+         sln = await sln;
+      }
+      console.log(sln);
       console.timeEnd(`solution ${part}`);
    };
 
    if (!partOrNot) {
-      run('1');
-      run('2');
+      await run('1');
+      await run('2');
    } else {
       const oneOrTwo = partOrNot;
       if (oneOrTwo !== '1' && oneOrTwo !== '2' && !isForce) {
          console.log('Number after dash must be 1 or 2. Make next arg `force` to override this.');
          process.exit();
       }
-      run(oneOrTwo);
+      await run(oneOrTwo);
    }
 };
 
-if (solutionArg === 'all') {
-   for (let i = 1; i <= 25; i++) {
-      console.log(`Day ${i}`);
-      runDay(`${i}`, '');
-      console.log('');
+(async () => {
+   if (solutionArg === 'all') {
+      for (let i = 1; i <= 25; i++) {
+         console.log(`Day ${i}`);
+         await runDay(`${i}`, '');
+         console.log('');
+      }
+   } else {
+      await runDay(...solutionArg.split('-'));
    }
-} else {
-   runDay(...solutionArg.split('-'));
-}
-
-console.timeEnd('full run');
+   console.timeEnd('full run');
+})();
